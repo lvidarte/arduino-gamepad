926c152e4bc705ab845afe21389799111b6503a8
Leo Vidarte <lvidarte@gmail.com>
2014-08-18 23:48:12 -0300

    Added class Game and use example

--- gamepad.py
+++ gamepad.py
@@ -25,9 +25,12 @@ except OSError:
 
 class Event(object):
 
-    def __init__(self, cmd, param):
+    def __init__(self, cmd, x, y, switch, button):
         self.cmd = cmd
-        self.param = param
+        self.x = x
+        self.y = y
+        self.switch = switch
+        self.button = button
         self.name = self.get_name()
 
     def get_name(self):
@@ -35,28 +38,27 @@ class Event(object):
             return 'change-x'
         elif self.cmd == CMD_Y:
             return 'change-y'
-        elif self.cmd == CMD_SW and self.param == 0:
+        elif self.cmd == CMD_SW and self.switch == 0:
             return 'release-switch'
-        elif self.cmd == CMD_SW and self.param == 1:
+        elif self.cmd == CMD_SW and self.switch == 1:
             return 'press-switch'
-        elif self.cmd == CMD_BT and self.param == 0:
+        elif self.cmd == CMD_BT and self.button == 0:
             return 'release-button'
-        elif self.cmd == CMD_BT and self.param == 1:
+        elif self.cmd == CMD_BT and self.button == 1:
             return 'press-button'
 
 class Gamepad(object):
 
     def __init__(self, conn):
         self.conn = conn
+        self.x = None
+        self.y = None
+        self.switch = None
+        self.button = None
         self._callbacks = []
 
     def on(self, event, callback):
         self._callbacks.append({'event': event, 'f': callback})
-
-    def _run_callbacks(self, event):
-        for callback in self._callbacks:
-            if callback['event'] in (event.name, 'all'):
-                callback['f'](self, event)
 
     def get_cmd(self, data):
         return data & CMD_MASK
@@ -64,59 +66,66 @@ class Gamepad(object):
     def get_param(self, data):
         return data & PARAM_MASK
 
-    def is_x(self, cmd):
-        return cmd == CMD_X
-
-    def is_y(self, cmd):
-        return cmd == CMD_Y
-
-    def is_switch(self, cmd):
-        return cmd == CMD_SW
-
-    def is_button(self, cmd):
-        return cmd == CMD_BT
-
     def get_cmd_name(self, cmd):
         return CMD_NAMES[cmd >> 6]
+
+    def set(self, data):
+        self.cmd = self.get_cmd(data)
+        self.param = self.get_param(data)
+        if self.cmd == CMD_X:
+            self.x = self.param
+        if self.cmd == CMD_Y:
+            self.y = self.param
+        if self.cmd == CMD_SW:
+            self.switch = self.param
+        if self.cmd == CMD_BT:
+            self.button = self.param
+
+    def get_event(self):
+        return Event(self.cmd,
+                     self.x,
+                     self.y,
+                     self.switch,
+                     self.button)
+
+    def run_callbacks(self, event):
+        for callback in self._callbacks:
+            if callback['event'] in (event.name, 'all'):
+                callback['f'](event)
 
     def listen(self):
         while True:
             data = ord(self.conn.read())
-            cmd = self.get_cmd(data)
-            param = self.get_param(data)
-            event = Event(cmd, param)
-            self._run_callbacks(event)
-            print "cmd: %s, param: %s" % (self.get_cmd_name(cmd), param)
+            self.set(data)
+            event = self.get_event()
+            self.run_callbacks(event)
 
 
 if __name__ == '__main__':
 
     gamepad = Gamepad(conn)
 
-    def press(self, event):
-        print "%s: %s, %s" % (
-            event.name, 
-            self.get_cmd_name(event.cmd),
-            event.param)
+    class Game:
 
-    def release(self, event):
-        print "%s: %s, %s" % (
-            event.name,
-            self.get_cmd_name(event.cmd),
-            event.param)
+        def __init__(self, gamepad):
+            self.gamepad = gamepad
+            self.x = 0
+            self.init()
 
-    def change(self, event):
-        print "%s: %s" % (event.name, event.param)
+        def button(self, event):
+            self.x += 1
+            print "%s, %s, %s" % (event.name, event.button, self.x)
 
-    def all(self, event):
-        print '-----------------------'
+        def move(self, event):
+            print "%s (%s, %s)" % (event.name, event.x, event.y)
 
-    gamepad.on('press-button', press)
-    gamepad.on('release-button', release)
-    gamepad.on('press-switch', press)
-    gamepad.on('release-switch', release)
-    gamepad.on('change-x', change)
-    gamepad.on('change-y', change)
-    gamepad.on('all', all)
+        def init(self):
+            self.gamepad.on('press-button', self.button)
+            self.gamepad.on('release-button', self.button)
+            self.gamepad.on('release-switch', self.button)
+            self.gamepad.on('change-x', self.move)
+            self.gamepad.on('change-y', self.move)
+            self.gamepad.listen()
 
-    gamepad.listen()
+    game = Game(gamepad)
+
