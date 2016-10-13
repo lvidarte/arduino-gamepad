b0a0f2342f898202bedc872c545cc67bd0aa75b1
Leo Vidarte <lvidarte@gmail.com>
2016-10-13 10:14:25 -0300

    Add gpl license

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,3 +1,13 @@
+/*
+Author: Leo Vidarte <http://nerdlabs.com.ar>
+
+This is free software:
+you can redistribute it and/or modify it
+under the terms of the GPL version 3
+as published by the Free Software Foundation.
+
+*/
+
 #define SERIAL_SPEED 9600
 
 #define PIN_X  A0
